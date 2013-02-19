7e56e2acfbfa151f16a12178a6ebc0ecc5922d26
Matthew King <mking@mking.info>
2013-02-19 23:00:38 +0800

    Updated build script and nuspec definition.

--- Build/Build.ps1
+++ Build/Build.ps1
@@ -10,6 +10,12 @@ New-Item -ItemType directory -Path .\Temp
 Add-Content .\Temp\AutofacExtensions.cs ((Get-Content .\EmbeddedHeader.txt | ForEach-Object { '// ' + $_ }) -replace '{version}', $version)
 Add-Content .\Temp\AutofacExtensions.cs ''
 Add-Content .\Temp\AutofacExtensions.cs ((Get-Content ..\Source\AutofacExtensions\AutofacExtensions.cs) -replace 'public static class', 'internal static class')
+Add-Content .\Temp\GlobalParameterModule.cs ((Get-Content .\EmbeddedHeader.txt | ForEach-Object { '// ' + $_ }) -replace '{version}', $version)
+Add-Content .\Temp\GlobalParameterModule.cs ''
+Add-Content .\Temp\GlobalParameterModule.cs ((Get-Content ..\Source\AutofacExtensions\GlobalParameterModule.cs) -replace 'public sealed class', 'internal sealed class')
+Add-Content .\Temp\GlobalPropertyModule.cs ((Get-Content .\EmbeddedHeader.txt | ForEach-Object { '// ' + $_ }) -replace '{version}', $version)
+Add-Content .\Temp\GlobalPropertyModule.cs ''
+Add-Content .\Temp\GlobalPropertyModule.cs ((Get-Content ..\Source\AutofacExtensions\GlobalPropertyModule.cs) -replace 'public sealed class', 'internal sealed class')
 Copy-Item ..\Source\AutofacExtensions\AutofacExtensions.Embedded.nuspec .\Temp\
 ..\Source\.nuget\NuGet.exe pack .\Temp\AutofacExtensions.Embedded.nuspec -OutputDirectory ..\Packages\
 Remove-Item .\Temp\ -Recurse
