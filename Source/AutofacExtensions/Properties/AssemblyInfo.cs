1eb733d94d4364e530ab5c432cef9978aac8617d
Matthew King <mking@mking.info>
2013-04-23 14:46:23 +0800

    Marked assembly as CLS Compliant.

--- Source/AutofacExtensions/Properties/AssemblyInfo.cs
+++ Source/AutofacExtensions/Properties/AssemblyInfo.cs
@@ -2,3 +2,4 @@
 [assembly: System.Reflection.AssemblyDescription("Extension methods for Autofac")]
 [assembly: System.Runtime.InteropServices.ComVisible(false)]
 [assembly: System.Runtime.InteropServices.Guid("7dd33ef3-56ec-4187-b870-5871a9efabe5")]
+[assembly: System.CLSCompliant(true)]
